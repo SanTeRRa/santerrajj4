f8dbf77ed3bde7d83e5d1d087a358273e9130541
Glitch (santerrajj4) <none>
2020-07-22 10:18:06 +0000

    🏐🕷 Updated with Glitch

--- README.md
+++
@@ -1,1 +0,0 @@
-# santerrajj4
---
+++ fyukas.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Oyuncular Şehri | J4J BOT");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000) 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const data = new Map();
+client.on('ready', () => {
+        console.log(`${client.user.username} ismi ile giriş yapıldı! ${client.guilds.size} Sunucu, ${client.users.size} Kullanıcı.`);
+});
+
+client.on("message", async msg => {
+  if (msg.channel.type === "dm") {
+    if (msg.author.id === client.user.id) {
+    } else {
+      if (msg.author.bot) {
+      } else {
+        let fyukas = await data.get(msg.author.id)
+        
+        if (fyukas === 1) {
+        } else {
+          await data.set(msg.author.id, 1)
+          await sleep(1500)
+          await msg.channel.send("J2J discord.gg/sn4QVXE") // Discord sunucunuzun kalıcı linkini girin size mesaj atanlara oto dönüş yapması için.      
+          await sleep(1500)
+          await msg.channel.send("Done/Tamam") // DM den mesaj gelince bot bu mesaj ile dönüş yapıcak      
+      }
+      }
+    }
+  }
+})
+
+
+client.on("guildCreate", fyukas => {
+  sleep(1000);
+  if (
+    fyukas.id === "561932459835392011" ||  // j4j sunucusunun idsi
+    fyukas.id === "561932459835392011"  // j4j sunucusunun idsi
+  ) {
+    
+  } else {
+    fyukas.leave();
+  }
+    
+})
+client.on("ready", () => {
+      setInterval(() => {
+       let a = client.channels.get("735426066021220403") // j4j sunucusunun kanal id'si
+
+
+       a.send("J2J seri") // 30 sn de girdiğiniz mesajı belirtilen kanala atar
+
+
+      }, 30000);
+})
+client.on("ready", () => {
+      setInterval(() => {
+       let a = client.channels.get("735426066021220403") // j4j sunucusunun kanal id'si
+
+
+       a.send("J2J DM") // 60 sn de girdiğiniz mesajı belirtilen kanala atar
+
+
+      }, 60000);
+})
+
+client.login("NzA5MDEzNTgwODYyNzE3OTcz.XxgH0A._moOrnEGb_uJC9UnlwcxMp18WC8") // user tokeniniz
+
